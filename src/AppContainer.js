ec926e6c2497458f9ce5387d6f4e391a0a9b8ca4
Aidan Taylor-Lynch <aidant@ocean.com.au>
2020-02-03 14:59:04 +1100

    add flexbox

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { Combobox, Button } from 'evergreen-ui';
 import './App.css';
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        {this.props.title}
+      <div className="appContainer">
+        <div className="title five-pix-pad">
+          {this.props.title}
+        </div>
+        <div className="comboboxContainer five-pix-pad">
+          <Combobox 
+            items={["Bob's eatery", "Mexican cantina", "Burger Werx"]}
+            placeholder="Select Venue"
+          />
+        </div>
+        <div className="buttonContainer five-pix-pad">
+          <Button appearance="minimal">Next</Button>
+        </div>
       </div>
     )
   }
