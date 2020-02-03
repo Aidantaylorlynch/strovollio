e29651e6912f610c5755e74329870e53348602cb
Aidan Taylor-Lynch <aidant@ocean.com.au>
2020-02-03 15:51:25 +1100

    add browser router basic as

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { Combobox, Button } from 'evergreen-ui';
+import { OrderContainer } from './OrderContainer';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 export class App extends Component {
   render() {
     return (
-      <div className="appContainer">
-        <div className="title five-pix-pad">
-          {this.props.title}
-        </div>
-        <div className="comboboxContainer five-pix-pad">
-          <Combobox 
-            items={["Bob's eatery", "Mexican cantina", "Burger Werx"]}
-            placeholder="Select Venue"
-          />
-        </div>
-        <div className="buttonContainer five-pix-pad">
-          <Button appearance="minimal">Next</Button>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <div className="appContainer">
+              <div className="title five-pix-pad">
+                {this.props.title}
+              </div>
+              <div className="comboboxContainer five-pix-pad">
+                <Combobox 
+                  items={["Bob's eatery", "Mexican cantina", "Burger Werx"]}
+                  placeholder="Select Venue"
+                />
+              </div>
+              <div className="buttonContainer five-pix-pad">
+                <Button is="a" href="/Order" appearance="minimal">Next</Button>
+              </div>
+            </div>
+          </Route>
+          <Route path="/Order">
+            <OrderContainer />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
---
+++ src/OrderContainer.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+export class Order extends Component {
+  render() {
+    return (
+      <div className="orderContainer">
+          Order
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    title: state.appTitle
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  }
+}
+
+export const OrderContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Order)
