f90545e1d2f2d4727cfe809cc2dab71d2630ca72
Aidan Taylor-Lynch <aidant@ocean.com.au>
2020-02-18 09:21:26 +1100

    change base url to use env

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const BASE_URL = 'http://localhost:5000/api'
+const BASE_URL = process.env['REACT_APP_BASE_URL'];
 
 export const createOrder = async (order) => {
     console.log(order)
